2c6931bef4e99aee8be59ce973255c0e9e41656b
Joyce Benson <bensonjoyce25@gmail.com>
2025-03-09 18:08:54 +0300

    initial commit

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,36 @@
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+    
+    def __init__(self, name, pet_type, owner=None):
+        self.name = name
+        
+        # Validate pet_type
+        if pet_type not in self.PET_TYPES:
+            raise Exception(f"Pet type must be one of {self.PET_TYPES}")
+        self.pet_type = pet_type
+        
+        # Set owner if provided
+        self.owner = owner
+        
+        # Add pet to all pets list
+        Pet.all.append(self)
 
 class Owner:
+    def __init__(self, name):
+        self.name = name
+    
+    def pets(self):
+        """Returns all pets owned by this owner"""
+        return [pet for pet in Pet.all if pet.owner == self]
+    
+    def add_pet(self, pet):
+        """Adds a pet to this owner after validating it's a Pet instance"""
+        if not isinstance(pet, Pet):
+            raise Exception("Only Pet instances can be added")
+        pet.owner = self
+    
+    def get_sorted_pets(self):
+        """Returns pets sorted by name"""
+        return sorted(self.pets(), key=lambda pet: pet.name)
     pass
